e9105ba4a5173d850d185c3eb056fca5fd01936c
Henry Kupty <hkupty@gmail.com>
2022-12-11 01:46:56 +0100

    feat: Add filter for kvs

--- src/carbon/util.clj
+++ src/carbon/util.clj
@@ -8,6 +8,14 @@
   ([-fn] (map (fn [^clojure.lang.MapEntry [k v]] [(-fn k) v])))
   ([-fn coll] (into {} (map-keys -fn) coll)))
 
+(defn filter-vals
+  ([-fn] (filter (comp -fn val)))
+  ([-fn coll] (into {} (filter-vals -fn) coll)))
+
+(defn filter-keys
+  ([-fn] (filter (comp -fn key)))
+  ([-fn coll] (into {} (filter-keys -fn) coll)))
+
 (defn with-options [args]
   (if (map? (first args))
            ((juxt first next) args)
