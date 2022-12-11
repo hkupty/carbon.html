48a787faaf58840ca76e19b9618a7b0f488ff1a7
Henry Kupty <hkupty@gmail.com>
2022-12-11 15:48:18 +0100

    test: Use process-tree instead of render
    Render will be lifted to translating to HTML

--- test/carbon/html_test.clj
+++ test/carbon/html_test.clj
@@ -15,21 +15,21 @@
 (deftest carbon-v2-syntax
   (testing :for
     (is (match? [:div [:p 1] [:p 2] [:p 3]]
-                (p/render '[:div [:for [i items] [:p i]]]
+                (tags/process-tree '[:div [:for [i items] [:p i]]]
                          {:items [1 2 3]}
                          syntax/default-tags))))
 
 
   (testing :if
     (is (match? [:div [:p "true branch"]]
-                  (p/render '[:div [:if is-it-true
+                  (tags/process-tree '[:div [:if is-it-true
                                    [:p "true branch"]
                                    [:p "false branch"]]]
                            {:is-it-true true}
                            syntax/default-tags)))
 
    (is (match? [:div [:p "false branch"]]
-                  (p/render '[:div [:if is-it-true
+                  (tags/process-tree '[:div [:if is-it-true
                                    [:p "true branch"]
                                    [:p "false branch"]]]
                            {:is-it-true false}
@@ -37,26 +37,26 @@
 
  (testing :when
     (is (match? [:div [:p "true branch"]]
-                  (p/render '[:div [:when is-it-true
+                  (tags/process-tree '[:div [:when is-it-true
                                    [:p "true branch"]]]
                            {:is-it-true true}
                            syntax/default-tags)))
 
     (is (match? [:div]
-                (p/render '[:div [:when is-it-true
+                (tags/process-tree '[:div [:when is-it-true
                                   [:p "true branch"]]]
                           {:is-it-true false}
                           syntax/default-tags))))
 (testing :component
    (testing "simple component"
      (is (match? [:div [:p "Amazing"]]
-               (p/render '[:div [:Component argument]]
+               (tags/process-tree '[:div [:Component argument]]
                          {:argument "Amazing"}
                          {:Component (tags/component ['base] [:p 'base])}))))
 
    (testing "custom function"
      (is (match? [:p 150]
-                   (p/render [:calc 30]
+                   (tags/process-tree [:calc 30]
                              {}
                              {:calc
                               (fn [[arg] _base-map _components]
@@ -64,47 +64,47 @@
 
  (testing "attribute metadata"
     (testing "empty metadata"
-      (is (match? [:p {} "text"] (p/render '[:p {} "text"] {} syntax/default-tags))))
+      (is (match? [:p {} "text"] (tags/process-tree '[:p {} "text"] {} syntax/default-tags))))
 
   (testing "static metadata"
-      (is (match? [:p {:x 1} "text"] (p/render '[:p {:x 1} "text"] {} syntax/default-tags))))
+      (is (match? [:p {:x 1} "text"] (tags/process-tree '[:p {:x 1} "text"] {} syntax/default-tags))))
 
   (testing "dynamic metadata"
     (is (match? [:p {:x "true"} "text"]
-                (p/render '[:p
+                (tags/process-tree '[:p
                             {:x [:if data "true" "false"]}
                             "text"]
                           {:data true}
                           syntax/default-tags)))
 
     (is (match? [:p {:x "something"} "text"]
-                (p/render '[:p [:when 'data
+                (tags/process-tree '[:p [:when 'data
                                 {:x "something"}]
                             "text"]
                           {:data true}
                           syntax/default-tags)))
 
     (is (match? [:p {:x "something"} "text"]
-                (p/render '[:p [:when data
+                (tags/process-tree '[:p [:when data
                                 [:merge {:x "discard"} {:x "something"}]]
                             "text"]
                           {:data true}
                           syntax/default-tags)))
 
     (is (match? [:p {:x "something"} "text"]
-                (p/render '[:p [:when data
+                (tags/process-tree '[:p [:when data
                                 nested]
                             "text"]
                           {:data true
                            :nested {:x "something"}}
                           syntax/default-tags)))
     (is (match? [:p "text"]
-                (p/render '[:p [:when ^:nullable? data
+                (tags/process-tree '[:p [:when ^:nullable? data
                                 "text"]]
                           {:data true}
                           syntax/default-tags)))
     (is (match? [:p]
-                (p/render '[:p [:when ^:nullable? data
+                (tags/process-tree '[:p [:when ^:nullable? data
                                 "text"]]
                           {}
                           syntax/default-tags))))))
